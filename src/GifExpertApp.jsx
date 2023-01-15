67074351657487d20cc75ac5f9e2d990554139c1
Xavier Puentestar <xavierpuentestar97@gmail.com>
2023-01-14 20:13:08 -0500

    Primer commit

---
+++ src/GifExpertApp.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import { AddCategory, GifGrid } from "./components";
+
+export const GifExpertApp = () => {
+    const [categories, setCategories] = useState(['Mob Psycho']);
+    const onAddCategory = (newCategory) => {
+
+        if (categories.includes(newCategory)) return;
+        setCategories([newCategory, ...categories]);
+    }
+    return (
+        <>
+            <h1>-- Gif - Exper - App--</h1>
+
+            <AddCategory
+                onNewCategory={(value) => onAddCategory(value)}
+            />
+            {
+                categories.map((category) => (
+                    <GifGrid key={category}
+                        category={category} />
+                ))
+            }
+        </>
+    )
+}
